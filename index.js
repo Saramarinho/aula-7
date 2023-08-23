3d13c327b78f8eb204de4b7db77439d4547f11a3
Saritah <142619907+Saramarinho@users.noreply.github.com>
2023-08-23 14:50:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+/*exercicio1
+*/
+let A = 3;
+let B = 4;
+console.log(A + B);
+
+let C = 3;
+let D = 5;
+let E = 2;
+let F = -1;
+console.log(C * D  / 2);
+console.log(D - B * F);
+let G = 234;
+let H = 5;
+console.log(G % H );
